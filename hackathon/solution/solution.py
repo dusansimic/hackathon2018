9ef0bce325db6256b7c847918f2a8aa2a2ea127e
Dušan Simić <dusan.simic1810@gmail.com>
2018-11-17 14:37:33 +0100

    Build 2
    Cost: 2184.4$ / Penalty: 105
    Ovaj je dosta optimizovao ukupnu cenu. Jos uvek se razvijaju stvari.

--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -10,11 +10,8 @@ def worker(msg: DataMessage) -> ResultsMessage:
     """TODO: This function should be implemented by contestants."""
     # Details about DataMessage and ResultsMessage objects can be found in /utils/utils.py
 
-    load_one = True
-    load_two = True
-    load_three = True
-    power_reference = 0.0
-    pv_mode = PVMode.ON
+    (power_reference, pv_mode) = ChargingHandler(msg)
+    (load_one, load_two, load_three, power_reference) = BlackoutHandler(msg, power_reference)
 
     # if msg.grid_status:
     #     if msg.bessSOC != 1 and msg.buying_price == msg.selling_price \
@@ -57,12 +54,58 @@ def run(args) -> None:
     for data in cntrl.get_data():
         cntrl.push_results(worker(data))
 
-def blackout_handler(msg):
-    if(msg.grid_status == False):
-        power_reference = msg.current_max_load - msg.solar_production
-        if(power_reference > 5):
-            power_reference = 5
+"""
+    Punjenje baterije
+    Params: msg
+    Returns: power_reference
+"""
+def ChargingHandler(msg):
+    power_reference = 0.0
+    pv_mode = PVMode.ON
+
+    # Ako udjemo u gap gde je struja skupa a nema svetla
+    if msg.solar_production == 0 and msg.buying_price == 8:
+        power_reference = 5.0
+    # Ako radi grid i napunjenost baterije je manje od 95%
+    if msg.grid_status and msg.bessSOC < 0.95 and msg.buying_price == 3:
+        power_reference = -5.0
+    if not msg.grid_status:
+        if msg.bessSOC >= 0.95:
+            pv_mode = PVMode.OFF
+        else:
+            pv_mode = PVMode.ON
+
+    return (power_reference, pv_mode)
+
+"""
+    Resavanje problema u slucaju blackouta
+    Params: msg
+    Returns: load_one, load_two, load_three, power_reference
+"""
+def BlackoutHandler(msg, power_reference):
+    load_one = True
+    load_two = True
+    load_three = True
+
+    # Ako nema grida
+    if not msg.grid_status:
+        # Povlacenje baterije je jednako opterecenju kuce bez panela
+        power_reference = msg.current_load - msg.solar_production
+        # Ako je opterecenje baterije vece od maksimalnog opterecenja
+        if(power_reference > 5.0):
+            power_reference = 5.0
+            # Gasenje uredjaja radi sprecavanja preopterecenja
+            (load_one, load_two, load_three) = LoadHandler(msg)
+
+    return (load_one, load_two, load_three, power_reference)
 
 
 def LoadHandler(msg):
-    if(blackout_flag and (msg.current_max_load > msg.solar_production + msg.bessPower)
+    # Ako je opterecenje kuce vece od baterije i panela zajedno
+    if msg.current_load > msg.solar_production + 5.0:
+        # Ako je opterecenje kuce bez 3. uredjaja vece od baterije i panela zajedno
+        if msg.current_load * 0.6 > msg.solar_production + msg.bessPower:
+            # Gasi se 2. i 3. uredjaj
+            return (True, False, False)
+        # Gasi se 3. uredjaj
+        return (True, True, False)
